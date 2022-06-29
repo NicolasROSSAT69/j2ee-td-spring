095702193581b91ebc54ab884c4e7406312e1a64
nicolas <nicolas.rossat@hotmail.fr>
2022-06-29 14:38:55 +0200

    Ajout de la dépendance springdoc- openapi-data-rest & ajout paramètres page, size, et sort.

--- src/main/java/com/j2ee/j2eetdspring/controllers/UserController.java
+++ src/main/java/com/j2ee/j2eetdspring/controllers/UserController.java
@@ -4,6 +4,8 @@ import com.j2ee.j2eetdspring.entities.User;
 import com.j2ee.j2eetdspring.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,24 +19,38 @@ public class UserController {
 
     @RequestMapping(path = "/{username}", method = RequestMethod.GET)
     public User get(@PathVariable(name = "username") String username) {
+
         return userService.getUserById(username);
+
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     public User createOrUpdate(@RequestBody User user) {
+
         return userService.createOrUpdate(user);
+
     }
 
     @Operation(summary = "Récupération de tous les utilisateurs")
     @RequestMapping(path = "/_all", method = RequestMethod.GET)
     public List<User> getAllUsers() {
+
         return userService.getAllUsers();
+
     }
 
     @Operation(summary = "Suppression d'un utilisateur à partir de son identifiant")
     @RequestMapping(path = "/{username}", method = RequestMethod.DELETE)
     public void deleteUser(@PathVariable(value = "username") String username) {
+
         userService.deleteUser(username);
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET) public Page<User> getUsers(Pageable pageable) {
+
+        return userService.getUsersWithPaging(pageable);
+
     }
 
 }
--- src/main/java/com/j2ee/j2eetdspring/services/UserService.java
+++ src/main/java/com/j2ee/j2eetdspring/services/UserService.java
@@ -4,6 +4,8 @@ import com.j2ee.j2eetdspring.entities.User;
 import com.j2ee.j2eetdspring.repositories.UserRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,18 +19,32 @@ public class UserService {
     private UserRepository userRepository;
 
     public User createOrUpdate(User user) {
+
         return userRepository.save(user);
+
     }
 
     public User getUserById(String username) {
+
         return userRepository.findById(username).orElse(null);
+
     }
 
     public List<User> getAllUsers() {
+
         return userRepository.findAll();
+
     }
 
     public void deleteUser(String username) {
+
         userRepository.deleteById(username);
+
+    }
+
+    public Page<User> getUsersWithPaging(Pageable pageable) {
+
+        return userRepository.findAll(pageable);
+
     }
 }
